ce4b97699ccd0c5c969f8dd1aa2a8499cce4a488
Lee Junseon <limazero14@gmail.com>
2022-04-19 03:07:40 +0900

    초기 커밋.

---
+++ BOJ_15650.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, Msize;
+
+void bricks(int num, vector<int>& v)
+{
+	if (v.size() == Msize) {
+		for (int& i : v) 
+			cout << i << " ";
+		cout << "\n";
+		return;
+	}
+	for (int i = num; i <= N; i++) {
+		v.push_back(i);
+		bricks(i + 1, v);
+		v.pop_back();
+	}
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	cin >> N >> Msize;
+	vector<int> v;
+	bricks(1, v);
+
+	return 0;
+}
